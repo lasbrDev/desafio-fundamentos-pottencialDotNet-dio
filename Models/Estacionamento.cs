5e7c1fccfa9e98a3990e67166a9f104ac40881db
lasbrDev <lasbr_alves@outlook.com>
2022-10-02 21:11:45 -0300

    first commit

---
+++ Models/Estacionamento.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioFundamentos.Models
+{
+  public class Estacionamento
+  {
+    private decimal precoInicial = 0;
+    private decimal precoPorHora = 0;
+    private List<string> veiculos = new List<string>();
+
+    public Estacionamento(decimal precoInicial, decimal precoPorHora)
+    {
+      this.precoInicial = precoInicial;
+      this.precoPorHora = precoPorHora;
+    }
+
+    public void AdicionarVeiculo()
+    {
+      // Implementado!!!!!
+      Console.WriteLine("Digite a placa do veículo para estacionar:");
+      veiculos.Add(Console.ReadLine()!.ToUpper());
+    }
+
+    public void RemoverVeiculo()
+    {
+      // Implementado!!!!!
+      Console.WriteLine("Digite a placa do veículo para remover:");
+      string placa = Console.ReadLine()!.ToUpper();
+
+      // Verifica se o veículo existe
+      if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+      {
+        // Implementado!!!!!
+        Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+        int horas = int.Parse(Console.ReadLine()!);
+        decimal valorTotal = precoInicial + precoPorHora * horas;
+        veiculos.Remove(placa);
+        Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+      }
+      else
+      {
+        Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+      }
+    }
+
+    public void ListarVeiculos()
+    {
+      // Verifica se há veículos no estacionamento
+      if (veiculos.Any())
+      {
+        // Implementado!!!!!
+        Console.WriteLine("Os veículos estacionados são:");
+        veiculos.ForEach(x => Console.WriteLine(x));
+      }
+      else
+      {
+        Console.WriteLine("Não há veículos estacionados.");
+      }
+    }
+  }
+}
